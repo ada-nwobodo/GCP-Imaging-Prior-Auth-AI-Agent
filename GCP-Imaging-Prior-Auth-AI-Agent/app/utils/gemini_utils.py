cee50d79252e8a366fc4822e701f8e92785e67dc
ada-nwobodo <blackgirldoctorcoder@gmail.com>
2025-07-01 12:55:41 +0000

    Fix: remove embedded Git repo and add directory as regular folder

---
+++ GCP-Imaging-Prior-Auth-AI-Agent/app/utils/gemini_utils.py
@@ -0,0 +1,2 @@
+def get_auth_response(input_text):
+    return f"You entered: {input_text}"
---
+++ GCP-Imaging-Prior-Auth-AI-Agent/utils/gemini_utils.py
@@ -0,0 +1,5 @@
+
+def get_auth_response(input_text)
+# This is a placeholder function
+return f"Authorization response for : {input_text}
+
